438e046eda4beb7746132c443a181a0bb8f9be03
A4Alumno <a4alumno@aules.local>
2018-04-25 16:00:07 +0200

    Verificar si campo de búsqueda vacío

--- js/search.js
+++ js/search.js
@@ -11,34 +11,37 @@ document.getElementById('icon_prefix2').addEventListener('keyup', function (even
 
     if (this.value) {
 
-
         for (let index = 0; index < listaManitas.length; index++) {
             if (listaManitas[index].toLowerCase().indexOf(this.value.toLowerCase()) >= 0) {
                 resultadoBusqueda.push(listaManitas[index]);
 
             } else { }
-
         }
 
-        console.log(resultadoBusqueda);
+        //console.log(resultadoBusqueda);
 
         plantillaLIs = '<ul>';
 
         for (let index = 0; index < resultadoBusqueda.length; index++) {
             plantillaLIs += `<li>${resultadoBusqueda[index]}</li>`;
-
         }
-
         plantillaLIs += '</ul>';
-
     } else { }
-
     sugerencias_div.innerHTML = plantillaLIs;
 });
 
-function validar(){
+/* function validar(){
     if ($('#icon_prefix2').text() == "") {
-        alert('Ingrese rut');
+        alert('El campo Búsqueda no puede estar vacío');
         return false;
     }
-};
+}; */
+
+   document.getElementById('search_button').onclick = function(){
+       if ($('#icon_prefix2').val() == "") {
+        $('#sugerencias').html('<p class="error">El campo de búsqueda no puede estar vacío</p>');
+       } else {
+        location.href='./search_result.html';
+       }
+   }
+
